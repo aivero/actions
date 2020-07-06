dc8ab8076d0a6c863b8b01d17e1539656bddd52f
Niclas Overby <noverby@prozum.dk>
2020-07-06 21:16:04 +0200

    Add support for uploading packages

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { inspect } from "util";
 import { promisify } from "util";
 import { spawn } from "child_process"
 import * as path from "path"
+import { stringify } from "querystring";
 
 function exec(full_cmd: string) {
   let args = full_cmd.split(' ');
@@ -17,16 +18,22 @@ async function run(): Promise<void> {
   try {
     const inputs = {
       package: core.getInput("package"),
-      version: core.getInput("version"),
       path: core.getInput("path"),
       profile: core.getInput("profile"),
+      conan_config: core.getInput("conan_config"),
+      conan_repo: core.getInput("conan_repo"),
     };
     core.debug(`Inputs: ${inspect(inputs)}`);
 
+    const [cfg_url, cfg_dir] = inputs.conan_config.split(':');
+    const [repo_name, repo_user, repo_password] = inputs.conan_repo.split(':')
+
     const conan_path = `${process.env.HOME}/.local/bin/conan`
-    exec(`${conan_path} config install https://github.com/aivero/conan-config/archive/master.zip -sf conan-config-master`);
+    exec(`${conan_path} config install ${cfg_url} -sf ${cfg_dir}`);
+    exec(`${conan_path} user ${repo_user} -p ${repo_password} -r ${repo_name}`);
     exec(`${conan_path} config set general.default_profile=${inputs.profile}`);
-    exec(`${conan_path} create ${inputs.path} ${inputs.package}/${inputs.version}@`);
+    exec(`${conan_path} create ${inputs.path} ${inputs.package}@`);
+    exec(`${conan_path} upload ${inputs.package} -r ${repo_name}`);
 
   } catch (error) {
     core.debug(inspect(error));
