f4616844d19d32f3e0bf847697413f2a48ce3dd5
Niclas Overby <noverby@prozum.dk>
2020-08-01 13:38:41 +0200

    Revert "Use x64 tag for x86_64 Linux builds"
    This reverts commit 0a080248b5f6a130bb96db2c1fc53000afb5a73a.

--- dist/index.js
+++ dist/index.js
@@ -5506,7 +5506,7 @@ function run() {
                             switch (os) {
                                 case "Linux":
                                     recipe.settings.arch_build.forEach((arch) => {
-                                        let tags = ["x64"];
+                                        let tags = ["ubuntu-18.04"];
                                         let image = "aivero/conan:bionic";
                                         if (arch == "armv8") {
                                             tags = ["ARM64"];
