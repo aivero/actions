dd4f1e04fdaf45a12e71c029ff6c09b9d0e62242
Niclas Overby <noverby@prozum.dk>
2020-11-02 22:02:57 +0100

    Cleanup code and separate conan specific code to classes

--- src/main.ts
+++ src/main.ts
@@ -1,126 +1,253 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-import simpleGit from "simple-git";
+import simpleGit, { SimpleGit } from "simple-git";
 import { inspect } from "util";
 import YAML from "yaml";
 import fs from "fs";
 import * as path from "path";
 import hash from "object-hash";
 
-async function package_find_build_hashes(
-  pkg: string,
-): Promise<{ [pkg: string]: Set<string> }> {
-  const [pkg_name, pkg_version] = pkg.split("/");
-  let build_hashes: { [pkg: string]: Set<string> } = {};
-  build_hashes[pkg_name] = new Set<string>();
-
-  const conf_path = path.join("recipes", pkg_name, "config.yml");
-  const file = fs.readFileSync(conf_path, "utf8");
-  const conf = YAML.parse(file);
-  conf.forEach((build) => {
-    if (pkg_version != "*" && pkg_version != build.version) {
-      return;
-    }
-    let pkg_hash = hash(build);
-    core.info(
-      `Build pkg/ver (hash): ${pkg_name}/${build.version} (${pkg_hash})`,
-    );
-    build_hashes[pkg_name].add(pkg_hash);
-  });
-
-  return build_hashes;
+const CONFIG_NAME = "config.yml";
+interface ConanConfig {
+  name?: string;
+  version?: string;
+  folder?: string;
+  profiles?: string[];
+  settings?: {};
+  options?: {};
+  image?: ImageConfig;
 }
-async function git_find_build_hashes(
-  repo_path: string,
-): Promise<{ [pkg: string]: Set<string> }> {
-  // Compare to previous commit
-  const git = simpleGit();
-  const diff = await git.diffSummary(["HEAD", "HEAD^"]);
+interface ImageConfig {
+  bootstrap?: boolean;
+}
 
-  // Find package versions that needs to be build
-  const build_hashes: { [pkg: string]: Set<string> } = {};
-  for (const f of diff.files) {
-    let file = f.file;
-    // Handle file renaming
-    if (file.includes(" => ")) {
-      core.info(`Renamed: ${file}`);
-      file = file.replace(/{(.*) => .*}/, "$1");
+
+class ConanMode {
+  subdir: string;
+
+  constructor(subdir) {
+    this.subdir = subdir;
+  }
+
+  async load_config_file(conf_path): Promise<Set<ConanConfig>> {
+    const name = path.basename(path.dirname(conf_path));
+    const conf_raw = fs.readFileSync(conf_path, "utf8");
+    return this.set_default_config(name, conf_raw)
+  }
+
+  async set_default_config(name, conf_raw): Promise<Set<ConanConfig>> {
+    let conf = YAML.parse(conf_raw) as [ConanConfig];
+    let disps = new Set<ConanConfig>();
+    conf.forEach((disp) => {
+      // Name
+      if (disp.name == undefined) {
+        disp.name = name;
+      }
+
+      // Default folder
+      let folder = "all";
+      if (disp.folder == undefined) {
+        disp.folder = folder;
+      }
+
+      // Default profiles
+      let profiles = [
+        "Linux-x86_64",
+        "Linux-armv8",
+      ];
+      if (disp.profiles == undefined) {
+        disp.profiles = profiles;
+      }
+
+      if (disp.image == undefined) {
+        disp.image = { boostrap: false } as ImageConfig;
+      }
+      disps.add(disp);
+    });
+
+    return disps;
+  }
+
+  async get_builder_options(disp: ConanConfig): Promise<{ tags; profile; image }[]> {
+    // Get build combinations
+    const builder_opts: { tags; profile; image }[] = [];
+    if (!disp.profiles) {
+      return builder_opts;
     }
-    const [root, pkg, conf_or_ver] = file.split("/");
+    disp.profiles.forEach((profile) => {
+      let image = "aivero/conan:";
+      let tags = ["x64"];
 
-    // Only handle changed files in recipe folder and
-    // only handle files that exist in current commit
-    if (root != "recipes" || !fs.existsSync(path.join(repo_path, file))) {
-      continue;
+      // OS options
+      if (profile.includes("musl")) {
+        image += "alpine";
+      } else if (profile.includes("Linux") || profile.includes("Wasi")) {
+        image += "bionic";
+      } else if (profile.includes("Windows")) {
+        image += "windows";
+      } else if (profile.includes("Macos")) {
+        image += "macos";
+      }
+
+      // Arch options
+      if (profile.includes("x86_64") || profile.includes("wasm")) {
+        image += "-x86_64";
+      } else if (profile.includes("armv8")) {
+        image += "-armv8";
+        tags = ["ARM64"];
+      }
+
+      // Handle bootstrap packages
+      if (disp.image && disp.image.bootstrap) {
+        image += "-bootstrap";
+      }
+
+      builder_opts.push({
+        tags: tags,
+        profile: profile,
+        image: image,
+      });
+    });
+    return builder_opts;
+  }
+
+}
+class ConanGitMode extends ConanMode {
+  rev: string;
+  git: SimpleGit;
+  repo_path: string;
+  disps: Set<ConanConfig>;
+
+  constructor(subdir, rev, repo_path = "") {
+    super(subdir)
+    this.subdir = subdir;
+    this.rev = rev;
+    this.repo_path = repo_path;
+    this.git = simpleGit(repo_path);
+    this.disps = new Set<ConanConfig>();
+  }
+
+  async find_config(dir): Promise<string> {
+    while (dir != this.subdir) {
+      let cfg_path = path.join(dir, CONFIG_NAME);
+      if (fs.existsSync(cfg_path)) {
+        return cfg_path;
+      }
+      dir = path.dirname(dir);
     }
+    return ""
+  }
 
-    // Create set with package versions to be build
-    if (!(pkg in build_hashes)) {
-      build_hashes[pkg] = new Set<string>();
-    }
+  async find_dispatces(): Promise<Set<ConanConfig>> {
+    // Compare to previous commit
+    const diff = await this.git.diffSummary(["HEAD", "HEAD^"]);
+    for (const f of diff.files) {
+      let file_path = f.file;
+      // Handle file renaming
+      if (file_path.includes(" => ")) {
+        core.info(`Renamed: ${file_path}`);
+        file_path = file_path.replace(/{(.*) => .*}/, "$1");
+      }
 
-    // Handle config.yml changes
-    if (conf_or_ver == "config.yml") {
-      // New config.yml
-      const conf_new = YAML.parse(await git.show(["HEAD:" + file]));
-      const files_old = await git.raw(["ls-tree", "-r", "HEAD^"]);
-      if (!files_old.includes(file)) {
-        core.info(`Created: ${pkg}/config.yml`);
-        conf_new.forEach((build) => {
-          let pkg_hash = hash(build);
-          core.info(
-            `Build pkg/ver (hash): ${pkg}/${build.version} (${pkg_hash})`,
-          );
-          build_hashes[pkg].add(pkg_hash);
-        });
+      // Only handle files that exist in current commit
+      if (!fs.existsSync(path.join(this.repo_path, file_path))) {
         continue;
       }
-      // Compare to old config.yml
-      core.info(`Changed: ${pkg}/config.yml`);
-      const conf_old = YAML.parse(await git.show(["HEAD^:" + file]));
-      conf_new.forEach((build) => {
-        // Check if build existed in old commit or if build data changed
-        let pkg_hash = hash(build);
-        let old_pkg_hashs = new Set<string>();
-        conf_old.forEach((build) => {
-          old_pkg_hashs.add(hash(build));
-        });
-        if (!old_pkg_hashs.has(pkg_hash)) {
-          core.info(
-            `Build pkg/ver (hash): ${pkg}/${build.version} (${pkg_hash})`,
-          );
-          build_hashes[pkg].add(pkg_hash);
-        }
-      });
-    } else {
-      // Handle {pkg-name}/{pkg-version}/* changes
-      const conf = YAML.parse(
-        await git.show([`HEAD:recipes/${pkg}/config.yml`]),
-      );
-      conf.forEach((build) => {
-        let folder = "all";
-        if ("folder" in build) {
-          folder = build.folder;
-        }
-        if (folder == conf_or_ver) {
-          let pkg_hash = hash(build);
-          core.info(
-            `Build pkg/ver (hash): ${pkg}/${build.version} (${pkg_hash})`,
-          );
-          build_hashes[pkg].add(pkg_hash);
-        }
-      });
-    }
-  }
-  return build_hashes;
-}
 
+      //const [root, pkg, conf_or_ver] = file.split("/");
+      const file = path.basename(file_path);
+      const file_dir = path.dirname(file_path);
+      const conf_path = await this.find_config(file_dir);
+      const name = path.basename(path.dirname(conf_path));
+
+      if (file == CONFIG_NAME) {
+        await this.handle_config_change(name, file_path);
+      } else {
+        await this.handle_file_change(name, conf_path, file_path);
+      }
+    }
+    return this.disps;
+  }
+
+  async handle_config_change(name, conf_path) {
+    // New config.yml
+    const conf_new = await this.set_default_config(name, await this.git.show([`HEAD:${conf_path}`]));
+    const files_old = await this.git.raw(["ls-tree", "-r", this.rev]);
+    if (!files_old.includes(conf_path)) {
+      core.info(`Created: ${conf_path}`);
+      conf_new.forEach((disp) => {
+        let disp_hash = hash(disp);
+        core.info(
+          `Dispatch name/version (hash): ${disp.name}/${disp.version} (${disp_hash})`,
+        );
+      });
+      this.disps = conf_new;
+      return;
+    }
+    // Compare to old config.yml
+    core.info(`Changed: ${conf_path}`);
+    const conf_old = await this.set_default_config(name, await this.git.show([`${this.rev}:${conf_path}`]));
+    conf_new.forEach((disp_new) => {
+      // Check if dispatch existed in old commit or if dispatch data changed
+      if (!conf_old.has(disp_new)) {
+        let disp_hash = hash(disp_new);
+        core.info(
+          `Dispatch name/version (hash): ${disp_new.name}/${disp_new.version} (${disp_hash})`,
+        );
+        this.disps.add(disp_new);
+      }
+    });
+  }
+
+  async handle_file_change(name, conf_path, file_path: string) {
+    const conf = await this.set_default_config(name, await this.git.show([`HEAD:${conf_path}`]));
+    conf.forEach((disp) => {
+      if (file_path.startsWith(path.join(this.subdir, name, disp.folder as string))) {
+        let disp_hash = hash(disp);
+        core.info(
+          `Dispatch name/version (hash): ${disp.name}/${disp.version} (${disp_hash})`,
+        );
+        this.disps.add(disp);
+      }
+    });
+  }
+}
+class ConanManualMode extends ConanMode {
+  disps: Set<ConanConfig>;
+
+  constructor(subdir) {
+    super(subdir);
+    this.disps = new Set<ConanConfig>();
+  }
+
+  async find_dispatces(pkg: string): Promise<Set<ConanConfig>> {
+    const [name, version] = pkg.split("/");
+
+    const conf_path = path.join(this.subdir, name, CONFIG_NAME);
+    const conf = await this.load_config_file(conf_path);
+
+    conf.forEach((disp) => {
+      if (version != "*" && version != disp.version) {
+        return;
+      }
+      let pkg_hash = hash(disp);
+      core.info(
+        `Build pkg/ver (hash): ${disp.name}/${disp.version} (${pkg_hash})`,
+      );
+      this.disps.add(disp);
+    });
+
+    return this.disps;
+  }
+}
 async function run(): Promise<void> {
   try {
     // Handle inputs
     const inputs = {
       token: core.getInput("token"),
       repository: core.getInput("repository"),
+      mode: core.getInput("mode"),
+      subdir: core.getInput("subdir"),
       repository_path: core.getInput("repository_path"),
       package: core.getInput("package"),
       arguments: core.getInput("arguments"),
@@ -128,149 +255,79 @@ async function run(): Promise<void> {
     core.debug(`Inputs: ${inspect(inputs)}`);
     const [owner, repo] = inputs.repository.split("/");
     const octokit = github.getOctokit(inputs.token);
-    const repo_path = inputs.repository_path || "";
 
-    let build_hashes: { [pkg: string]: Set<string> };
-    if (inputs.package) {
-      core.startGroup("Package Mode: Find build hashes that need to be build");
-      build_hashes = await package_find_build_hashes(inputs.package);
+    let disps: Set<ConanConfig>;
+    if (inputs.mode == "conan_git") {
+      core.startGroup("Conan Git Mode: Create dispatches from changed files in git");
+      let git_mode = new ConanGitMode(inputs.subdir, "^HEAD", inputs.repository_path);
+      disps = await git_mode.find_dispatces();
+      core.endGroup();
+    } else if (inputs.mode == "conan_manual") {
+      core.startGroup("Conan Manual Mode: Create dispatches from manual input");
+      let manual_mode = new ConanManualMode(inputs.subdir);
+      disps = await manual_mode.find_dispatces(inputs.package);
     } else {
-      core.startGroup("Git Mode: Find build hashes that need to be build");
-      build_hashes = await git_find_build_hashes(repo_path);
+      throw new Error(`Mode not supported: ${inputs.mode}`);
     }
-    core.endGroup();
 
     // Dispatch build for each build hash
-    for (const [pkg, pkg_hashes] of Object.entries(build_hashes)) {
-      const conf_path = path.join("recipes", pkg, "config.yml");
-      const file = fs.readFileSync(conf_path, "utf8");
-      const conf = YAML.parse(file);
-
-      for (const pkg_hash of pkg_hashes) {
-        const index = conf.findIndex((build) => hash(build) == pkg_hash);
-
-        // Name
-        let pkg_name = pkg;
-        if ("name" in conf[index]) {
-          pkg_name = conf[index].name;
+    disps.forEach(async (disp) => {
+      // Arguments
+      let args = inputs.arguments;
+      if (disp.settings) {
+        for (const [set, val] of Object.entries(disp.settings)) {
+          args += ` ${disp.name}:${set}=${val}`;
         }
-
-        // Version
-        let version = conf[index].version;
-
-        // Default folder
-        let folder = "all";
-        if ("folder" in conf[index]) {
-          folder = conf[index].folder;
-        }
-
-        // Default profiles
-        let profiles = [
-          "Linux-x86_64",
-          "Linux-armv8",
-        ];
-        if ("profiles" in conf[index]) {
-          profiles = conf[index].profiles;
-        }
-
-        // Image config
-        let bootstrap = false;
-        if ("image" in conf[index] && "bootstrap" in conf[index].image) {
-          bootstrap = conf[index].image.bootstrap;
-        }
-
-        // Settings
-        let settings = "";
-        if ("settings" in conf[index]) {
-          for (const [set, val] of Object.entries(conf[index].settings)) {
-            settings += `${pkg_name}:${set}=${val};`;
-          }
-          // Remove last :
-          settings = settings.slice(0, -1);
-        }
-        // Options
-        let options = "";
-        if ("options" in conf[index]) {
-          for (let [opt, val] of Object.entries(conf[index].options)) {
-            // Convert to Python bool
-            if (val == true) {
-              val = "True";
-            }
-            if (val == false) {
-              val = "False";
-            }
-            options += `${pkg_name}:${opt}=${val};`;
-          }
-          // Remove last :
-          options = options.slice(0, -1);
-        }
-        // Get build combinations
-        const combinations: { tags; profile; image }[] = [];
-        profiles.forEach((profile) => {
-          let image = "aivero/conan:";
-          let tags = ["x64"];
-
-          // OS options
-          if (profile.includes("musl")) {
-            image += "alpine";
-          } else if (profile.includes("Linux") || profile.includes("Wasi")) {
-            image += "bionic";
-          } else if (profile.includes("Windows")) {
-            image += "windows";
-          } else if (profile.includes("Macos")) {
-            image += "macos";
-          }
-
-          // Arch options
-          if (profile.includes("x86_64") || profile.includes("wasm")) {
-            image += "-x86_64";
-          } else if (profile.includes("armv8")) {
-            image += "-armv8";
-            tags = ["ARM64"];
-          }
-
-          // Handle bootstrap packages
-          if (bootstrap) {
-            image += "-bootstrap";
-          }
-
-          combinations.push({
-            tags: tags,
-            profile: profile,
-            image: image,
-          });
-        });
-
-        // Dispatch Conan events
-        core.startGroup("Dispatch Conan Events");
-        combinations.forEach(async (comb) => {
-          const payload = {
-            package: `${pkg_name}/${version}`,
-            args: inputs.arguments,
-            settings: settings,
-            options: options,
-            path: path.join("recipes", pkg, folder),
-            tags: comb.tags,
-            profile: comb.profile,
-            docker_image: comb.image,
-            ref: process.env.GITHUB_REF,
-            sha: process.env.GITHUB_SHA,
-          };
-          core.info(`${inspect(payload)}`);
-          await octokit.repos.createDispatchEvent({
-            owner: owner,
-            repo: repo,
-            event_type: `${pkg_name}/${version}: ${comb.profile}`,
-            client_payload: payload,
-          });
-        });
-        core.endGroup();
       }
+      // Options
+      if (disp.options) {
+        for (let [opt, val] of Object.entries(disp.options)) {
+          // Convert to Python bool
+          if (val == true) {
+            val = "True";
+          }
+          if (val == false) {
+            val = "False";
+          }
+          args += ` ${disp.name}:${opt}=${val}`;
+        }
+      }
+
+      let builder_opts = await get_builder_options(disp);
+
+      let mode_data = {
+        package: `${disp.name}/${disp.version}`,
+        args: args,
+        path: path.join(inputs.subdir, disp.name as string, disp.folder as string),
+      }
+
+      // Dispatch Conan events
+      core.startGroup("Dispatch Conan Events");
+      builder_opts.forEach(async (opt) => {
+        const payload = {
+          mode: inputs.mode,
+          mode_data: mode_data,
+          builder_options: opt,
+          ref: process.env.GITHUB_REF,
+          sha: process.env.GITHUB_SHA,
+        };
+        core.info(`${inspect(payload)}`);
+        //await octokit.repos.createDispatchEvent({
+        //  owner: owner,
+        //  repo: repo,
+        //  event_type: `${disp.name}/${disp.version}: ${opt.profile}`,
+        //  client_payload: payload,
+        //});
+      });
+      core.endGroup();
     }
+    )
   } catch (error) {
     core.debug(inspect(error));
     core.setFailed(error.message);
   }
 }
+
+
 
 run();
