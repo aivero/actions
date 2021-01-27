547e6e7e76ea9e015b9cf82da9224d600ab4a72a
Niclas Overby <noverby@prozum.dk>
2021-01-27 22:40:12 +0100

    Handle dispatch commands input

--- src/main.ts
+++ src/main.ts
@@ -2,47 +2,53 @@ import * as core from "@actions/core";
 import { inspect } from "util";
 import { spawn } from "child_process";
 import * as coreCommand from "@actions/core/lib/command";
-import YAML from "yaml";
-import path from "path";
-import fs from "fs";
-import os from "os";
-
-function sleep(millis) {
-  return new Promise((resolve) => setTimeout(resolve, millis));
-}
+import { env } from "process";
 
 async function exec(
   full_cmd: string,
-  fail_on_error = true,
-  return_stdout = false,
   env = process.env,
 ) {
+  core.startGroup(`Running command: '${full_cmd}'`);
+  // Replace env vars in command
+  full_cmd = full_cmd.replace(/\$[a-zA-Z0-9_]*/g, match => {
+    const env_var = match.substring(1);
+    return env[env_var] || "undefined";
+  });
+
+  // Handle assignment 
+  let match = full_cmd.match(/([a-zA-Z0-9_]*)=(.*)/)
+  if (match) {
+    env[match[1]] = match[2];
+    core.endGroup();
+    return;
+  }
+
+  // Handle change directory
+  match = full_cmd.match(/cd (.*)/)
+  if (match) {
+    env["CWD"] = match[1]; 
+    core.endGroup();
+    return;
+  }
+
   let args = full_cmd.split(" ");
   let cmd = args.shift();
   if (!cmd) {
     throw new Error(`Invalid command: '${full_cmd}'`);
   }
-  core.startGroup(`Running command: '${full_cmd}'`);
   const child = await spawn(
     cmd,
     args,
-    { stdio: ["ignore", "pipe", "pipe"], env: env },
+    { stdio: ["ignore", "pipe", "pipe"], env: env, cwd: env["CWD"] },
   );
 
   child.stderr.on("data", (data) => {
-    if (fail_on_error) {
-      core.error(data.toString("utf8"));
-    } else {
-      core.info(data.toString("utf8"));
-    }
+    core.error(data.toString("utf8"));
   });
 
   let res = "";
   for await (const chunk of child.stdout) {
     core.info(chunk);
-    if (return_stdout) {
-      res += chunk;
-    }
   }
   core.endGroup();
 
@@ -50,137 +56,61 @@ async function exec(
     child.on("close", resolve);
   });
 
-  if (exit_code && fail_on_error) {
+  if (exit_code) {
     throw new Error(`Command '${full_cmd}' failed with code: ${exit_code}`);
   }
-  return res.trim();
 }
 
-async function get_pkg_info(name: string, version: string, args: string) {
-  const file = `/tmp/${name}.json`;
-  await exec(`conan info ${args}${name}/${version}@ --paths --json ${file}`);
-  const pkg_info_json = fs.readFileSync(file, "utf8");
-  return JSON.parse(pkg_info_json);
+interface Inputs {
+  cmds?: [string],
+  cmdsPost?: [string],
+  env: {string: string};
 }
 
-async function upload_pkg(
-  name: string,
-  version: string,
-  args: string,
-  repo: string,
-  force_upload = true,
-) {
-  const info = await get_pkg_info(name, version, args);
-  const pkg_path = info[0].package_folder;
-  // Only upload if package is not empty (Empty packages contain 2 files: conaninfo.txt and conanmanifest.txt)
-  if (force_upload || fs.readdirSync(pkg_path).length > 2) {
-    await exec(
-      `conan upload ${name}/${version}@ --all -c -r ${repo}`,
-    );
+async function runCmds(cmds: [string], env: {string: string}) {
+  try {
+    for (const cmd of cmds) {
+      await exec(cmd, env);
+    };
+    
+  } catch (error) {
+    core.warning(error.message);
   }
 }
 
 async function run(): Promise<void> {
-  // Always run post
-  coreCommand.issueCommand("save-state", { name: "isPost" }, "true");
-  try {
-    const inputs = {
-      package: core.getInput("package"),
-      args: core.getInput("args"),
-      path: core.getInput("path"),
-      profile: core.getInput("profile"),
-    };
-    core.info(`Inputs: ${inspect(inputs)}`);
-
-    // Store package data in environment variables, so they can be used by later Github actions
-    let [name, version] = inputs.package.split("/");
-    core.exportVariable("CONAN_PKG_NAME", name);
-    core.exportVariable("CONAN_PKG_VERSION", version);
-    const conan_data_path = await exec(
-      "conan config get storage.path",
-      true,
-      true,
-    );
-    core.exportVariable("CONAN_DATA_PATH", conan_data_path);
-    const conan_pkg_path = path.join(conan_data_path, name, version, "_", "_");
-    core.exportVariable("CONAN_PKG_PATH", conan_pkg_path);
-
-    // Conan Setup
-    await exec(
-      `conan config install ${process.env.CONAN_CONFIG_URL} -sf ${process.env.CONAN_CONFIG_DIR}`,
-    );
-    await exec(
-      `conan user ${process.env.CONAN_LOGIN_USERNAME} -p ${process.env.CONAN_LOGIN_PASSWORD} -r ${process.env.CONAN_REPO_ALL}`,
-    );
-    await exec(
-      `conan user ${process.env.CONAN_LOGIN_USERNAME} -p ${process.env.CONAN_LOGIN_PASSWORD} -r ${process.env.CONAN_REPO_INTERNAL}`,
-    );
-    await exec(
-      `conan user ${process.env.CONAN_LOGIN_USERNAME} -p ${process.env.CONAN_LOGIN_PASSWORD} -r ${process.env.CONAN_REPO_PUBLIC}`,
-    );
-    await exec(`conan config set general.default_profile=${inputs.profile}`);
-
-    // Workaround to force fetch source until fixed upstream in Conan: https://github.com/conan-io/conan/issues/3084
-    await exec(`rm -rf ${path.join(conan_pkg_path, "source")}`);
-
-    // Set number of cores (AWS prevents Conan from detecting number of cores)
-    let env = Object.create(process.env);
-    env.CONAN_CPU_COUNT = os.cpus().length;
-
-    // Conan create
-    let args = inputs.args;
-    if (inputs.args) {
-        args = `${inputs.args.trim()} `;
-    };
-    await exec(
-      `conan create -u ${args}${inputs.path} ${name}/${version}@`,
-      true,
-      false,
-      env,
-    );
-    await exec(
-      `conan create -u ${args}${inputs.path} ${name}-dbg/${version}@`,
-      true,
-      false,
-      env,
-    );
-
-    // Select internal or public Conan repository according to license
-    const recipe = YAML.parse(
-      await exec(
-        `conan inspect ./${inputs.path}`,
-        true,
-        true,
-      ),
-    );
-    let conan_repo = process.env.CONAN_REPO_PUBLIC;
-    if (recipe["license"].includes("Proprietary")) {
-      conan_repo = process.env.CONAN_REPO_INTERNAL;
-    }
-    if (!conan_repo) {
-      throw new Error(`No upload Conan repository set`);
-    }
-
-    // Conan upload
-    await upload_pkg(name, version, args, conan_repo);
-    await upload_pkg(`${name}-dbg`, version, args, conan_repo);
-  } catch (error) {
-    core.debug(inspect(error));
-    core.setFailed(error.message);
+  const inputs: Inputs = {
+      cmds: JSON.parse(core.getInput("cmds")),
+      env: JSON.parse(core.getInput("env")),
   }
+  core.info(`Inputs: ${inspect(inputs)}`);
+
+  if (core.getInput("cmdPost")) {
+    coreCommand.issueCommand("save-state", { name: "cmdsPost" }, core.getInput("cmdPost"));
+  }
+  
+  if (!inputs.cmds) {
+    return;
+  }
+  await runCmds(inputs.cmds, inputs.env)
 }
 
 async function post(): Promise<void> {
   try {
-    await exec(`conan remove --locks`);
-    await exec(`conan remove * -f`);
+    const inputs: Inputs = {
+        cmdsPost: JSON.parse(core.getInput("cmdsPost")),
+        env: JSON.parse(core.getInput("env")),
+    }
+    for (let cmd in inputs.cmdsPost) {
+      await exec(cmd);
+    };
   } catch (error) {
     core.warning(error.message);
   }
 }
 
 // Main
-if (!process.env["STATE_isPost"]) {
+if (!process.env["STATE_cmdsPost"]) {
   run();
 } // Post
 else {
